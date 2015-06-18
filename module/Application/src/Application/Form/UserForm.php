28ab7de6c247bd582b3a1c1ff00f8ddcd0a86209
Zenologo <ppc2006.yan@gmail.com>
2015-06-18 12:32:06 +0200

    Update UserForm.php

--- module/Application/src/Application/Form/UserForm.php
+++ module/Application/src/Application/Form/UserForm.php
@@ -12,7 +12,66 @@ class UserForm extends Form
       'name'  => 'id',
       'type'  => 'Hidden',
     ));
+    
+    $this->add(array(
+      'name'    => 'nom',
+      'type'    => 'Text',
+      'option'  => array(
+        'label' => 'Nom',
+      ),
+    ));
+    
+    $this->add(arry(
+      'name'    => 'prenom',
+      'type'    => 'Text',
+      'option'  => array(
+        'label' => 'Prenom',
+      ),
+    ));
+    
+    
+    $this->add(arry(
+      'name'    => 'adresse',
+      'type'    => 'Text',
+      'option'  => array(
+        'label' => 'Adresse',
+      ),
+    ));
+    
+    $this->add(arry(
+      'name'    => 'telephone',
+      'type'    => 'Text',
+      'option'  => array(
+        'label' => 'Telephone',
+      ),
+    ));
+    
+    
+    $this->add(arry(
+      'name'    => 'email',
+      'type'    => 'Text',
+      'option'  => array(
+        'label' => 'Email',
+      ),
+    ));
+    
+    
+    $this->add(arry(
+      'name'    => 'pay',
+      'type'    => 'Text',
+      'option'  => array(
+        'label' => 'Pay',
+      ),
+    ));
+    
+    
+    $this->add(arry(
+      'name'    => 'pwd',
+      'type'    => 'Text',
+      'option'  => array(
+        'label' => 'Password',
+      ),
+    ));
+    
   }
-
-
 }
