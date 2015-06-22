72cad1952b368e3d9ed18e6e15f84825c63e838f
Zenologo <ppc2006.yan@gmail.com>
2015-06-22 15:39:19 +0200

    Update IndexController.php
    Add indexAction

--- module/Login/src/Login/Controller/IndexController.php
+++ module/Login/src/Login/Controller/IndexController.php
@@ -5,11 +5,20 @@ use Zend\Mvc\Controller\AbstractActionController;
 
 class IndexController extends AbstractActionController
 {
+    
+    protected $loginService;
 
+    public function __construction(LoginServiceInterface $loginService)
+    {
+        $this->loginService = $loginService;    
+    }
+    
+    
     public function indexAction()
     {
-    	
-        
+    	return new ViewModel (array(
+    	    'users' => $this->loginService->findAllUses()
+    	));
     }
 
 
